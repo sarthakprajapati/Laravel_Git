154992d9b73dfee122579aadfb6469dc2c6103a6
Sarthak Prajapati <sarthakprajapati@live.in>
2018-10-21 03:13:48 +0530

    Routes

--- –-prefer-dist/routes/web.php
+++ –-prefer-dist/routes/web.php
@@ -14,3 +14,18 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Route Parameters -> Required Parameters
+Route::get('/reqpar/{id}',function($id){
+	echo "<h1>Required Parameters</h1>";
+	echo '<h3>ID:'.$id.'</h3>';
+});
+
+//Route Parameters -> Optional Parameters
+Route::get('optpar/{name?}',function($name = 'sarthak_ishu11'){
+	echo '<h1>Optional Parameter</h1>';
+	echo '<h1>'.$name.'</h1>';
+});
+
+//Named Rote -- !Doubt
+Route::get('user/profile', 'UserController@showProfile')->name('profile');
